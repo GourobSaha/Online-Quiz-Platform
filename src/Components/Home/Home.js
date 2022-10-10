904358be94d1f5385f9321e64fbb9e0045af9401
Gourob Saha <gourobsaha009@gmail.com>
2022-10-10 23:40:13 +0600

    Loader Added & Data Loaded

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Quiz from '../Quiz/Quiz';
 
 const Home = () => {
+    const quizzes = useLoaderData();
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {
+                quizzes.data.map(quiz => <Quiz
+                    key={quiz.id}
+                    quiz={quiz}
+                ></Quiz>)
+            }
         </div>
     );
 };
---
+++ src/Components/Quiz/Quiz.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {ArrowRightCircleIcon } from '@heroicons/react/24/solid'
+
+const Quiz = ({quiz}) => {
+    const {name, logo, total} = quiz
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl mx-auto">
+            <figure className="px-10 pt-10">
+                <img src={logo} alt="Shoes" className="rounded-xl bg-slate-500" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title text-2xl mb-3 text-sky-900">{name}</h2>
+                <div className="card-actions flex items-center gap-6">
+                    <h3 className='font-semibold text-sky-900'>Total Questions: {total}</h3>
+                    <button className="btn bg-orange-600 border-0">Start Quiz <ArrowRightCircleIcon className='w-6 h-6 ml-1'/></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Quiz;
